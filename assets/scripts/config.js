f3a29e7a74c232696c4b0d9083522210ae420b8d
TimA89 <timabuladze@gmail.com>
2018-05-11 10:35:50 -0400

    Updated link in config.js

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://bucket-list-wdi.herokuapp.com/',
+  production: 'https://bucket-list-wdi.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
